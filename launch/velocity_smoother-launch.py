05eb31fba1448999f3ec26129fd1080b583d5aa0
bot-lin <zlin6364@hotmail.com>
2022-12-05 09:14:35 +0800

    Update velocity_smoother-launch.py

--- launch/velocity_smoother-launch.py
+++ launch/velocity_smoother-launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     # composed launch file similar, we use that style here as well.
     params_file = os.path.join(share_dir, 'config', 'velocity_smoother_params.yaml')
     with open(params_file, 'r') as f:
-        params = yaml.safe_load(f)['velocity_smoother']['ros__parameters']
+        params = yaml.safe_load(f)['kobuki_velocity_smoother']['ros__parameters']
     velocity_smoother_node = launch_ros.actions.Node(
         package='kobuki_velocity_smoother',
         executable='velocity_smoother_node',
