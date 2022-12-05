54f418df145a5be7dfd87b815be3aee621cc669c
bot-lin <zlin6364@hotmail.com>
2022-12-05 09:18:39 +0800

    Update velocity_smoother-composed-launch.py

--- launch/velocity_smoother-composed-launch.py
+++ launch/velocity_smoother-composed-launch.py
@@ -19,14 +19,14 @@ import yaml
 
 
 def generate_launch_description():
-    share_dir = ament_index_python.packages.get_package_share_directory('velocity_smoother')
+    share_dir = ament_index_python.packages.get_package_share_directory('kobuki_velocity_smoother')
 
     # Passing parameters to a composed node must be done via a dictionary of
     # key -> value pairs.  Here we read in the data from the configuration file
     # and create a dictionary of it that the ComposableNode will accept.
     params_file = os.path.join(share_dir, 'config', 'velocity_smoother_params.yaml')
     with open(params_file, 'r') as f:
-        params = yaml.safe_load(f)['velocity_smoother']['ros__parameters']
+        params = yaml.safe_load(f)['kobuki_velocity_smoother']['ros__parameters']
     container = ComposableNodeContainer(
             name='velocity_smoother_container',
             namespace='',
@@ -34,9 +34,9 @@ def generate_launch_description():
             executable='component_container',
             composable_node_descriptions=[
                 ComposableNode(
-                    package='velocity_smoother',
-                    plugin='velocity_smoother::VelocitySmoother',
-                    name='velocity_smoother',
+                    package='kobuki_velocity_smoother',
+                    plugin='kobuki_velocity_smoother::VelocitySmoother',
+                    name='kobuki_velocity_smoother',
                     parameters=[params]),
             ],
             output='both',
